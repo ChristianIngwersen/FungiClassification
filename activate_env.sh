79dc7c8d5bb42337ce9dc577353b537ada50e2a6
Niels Jeppesen <niejep@dtu.dk>
2022-08-16 13:22:50 +0200

    Added script to activate modules and env.

---
+++ activate_env.sh
@@ -0,0 +1,2 @@
+module load scipy/1.7.3-python-3.9.11 matplotlib/3.5.1-numpy-1.22.2-python-3.9.11 pandas/1.4.1-python-3.9.11 cuda/11.6
+source /work1/niejep/SummerSchoolChallenge2022/env/bin/activate
